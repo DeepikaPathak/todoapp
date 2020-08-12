db30390e7c175d9e7819b05a9d968c65aace1765
DeepikaPathak <65222334+DeepikaPathak@users.noreply.github.com>
2020-08-13 02:18:56 +0530

    Update footer.php

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,4 +1,4 @@
- <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>  
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>  
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> 
 <!-- <script src="<?= base_url("Assets/js/bootstrap.js")?>"></script>  -->
 <footer style=" position: fixed;
