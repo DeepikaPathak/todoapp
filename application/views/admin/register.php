6b398e8dc1f5c6b50a6868b091be00a6637bf6b5
DeepikaPathak <65222334+DeepikaPathak@users.noreply.github.com>
2020-08-13 02:22:17 +0530

    Update register.php

--- application/views/admin/register.php
+++ application/views/admin/register.php
@@ -1,5 +1,5 @@
 <?php include('header.php'); ?>
-<div class="container" style="margin-top: 50px;">
+<div class="container" style="margin-top: 25px;">
 	<div class="alert alert-info">
 	<h3>Registration Form</h3>
 </div>
@@ -12,28 +12,18 @@
 		
 
 			<div class="form-group">
-				<label for="firstname"> First Name</label>
+				<label for="firstname">Name</label>
 				<div class="row">
 					<div class="col-lg-6">
-				<?php echo form_input(['class'=>'form-control','type'=>'text','id'=>'firstname','placeholder'=>'Enter your First Name', 'name'=>'firstname','value'=>set_value('firstname')]);?>
+				<?php echo form_input(['class'=>'form-control','type'=>'text','id'=>'firstname','placeholder'=>'Enter your First Name', 'name'=>'name','value'=>set_value('name')]);?>
 					</div>
 					<div class="col-lg-6" style="margin-top: 7px">  
-						<?php echo form_error('firstname'); ?>
+						<?php echo form_error('name'); ?>
 					</div>
 				</div>	
 			</div>
 
-			<div class="form-group">
-				<label for="lastname"> Last Name</label>
-				<div class="row">
-					<div class="col-lg-6">
-				<?php echo form_input(['class'=>'form-control','type'=>'text','id'=>'lastname','placeholder'=>'Enter your Last Name', 'name'=>'lastname','value'=>set_value('lastname')]);?>
-					</div>
-					<div class="col-lg-6" style="margin-top: 7px">  
-						<?php echo form_error('lastname'); ?>
-					</div>
-				</div>	
-			</div>
+			
 
 			<div class="form-group">
 				<label for="email"> Email</label>
@@ -47,17 +37,7 @@
 				</div>	
 			</div>
 
-			<div class="form-group">
-				<label for="password"> Password</label>
-				<div class="row">
-					<div class="col-lg-6">
-				<?php echo form_password(['class'=>'form-control','type'=>'password','id'=>'password','placeholder'=>'Enter your Password', 'name'=>'password','value'=>set_value('password')]);?>
-					</div>
-					<div class="col-lg-6" style="margin-top: 7px">  
-						<?php echo form_error('password'); ?>
-					</div>
-				</div>	
-			</div>
+			
 
 			<div class="form-group">
 				<label for="mobile"> Mobile</label>
@@ -70,7 +50,41 @@
 					</div>
 				</div>		
 			</div>
-			
+
+			<div class="form-group">
+				<label for="body"> Date of Birth</label>
+				<div class="row">
+					<div class="col-lg-6">
+				<?php echo form_input(['class'=>'form-control','type'=>'date','id'=>'body','placeholder'=>'Enter your Date of Birth', 'name'=>'dob','value'=>set_value('dob')]);?>
+					</div>
+					<div class="col-lg-6" style="margin-top: 7px">  
+						<?php echo form_error('dob'); ?>
+					</div>
+				</div>	
+			</div>
+			<div class="form-group">
+				<label for="mobile"> Pin Code</label>
+				<div class="row">
+					<div class="col-lg-6">
+				<?php echo form_input(['class'=>'form-control','id'=>'mobile','placeholder'=>'Enter your Pin Code', 'name'=>'pincode','value'=>set_value('pincode')]);?>
+					</div>
+					<div class="col-lg-6" style="margin-top: 7px">  
+						<?php echo form_error('pincode'); ?>
+					</div>
+				</div>		
+			</div>
+			<div class="form-group">
+				<label for="password"> Password</label>
+				<div class="row">
+					<div class="col-lg-6">
+				<?php echo form_password(['class'=>'form-control','type'=>'password','id'=>'password','placeholder'=>'Enter your Password', 'name'=>'password','value'=>set_value('password')]);?>
+					</div>
+					<div class="col-lg-6" style="margin-top: 7px">  
+						<?php echo form_error('password'); ?>
+					</div>
+				</div>	
+			</div>
+
 			<div class="form-group">
 				<?php echo form_submit(['type'=>'submit','value'=>'Register','class'=>'btn btn-success']); ?>
 				<?php echo form_reset(['type'=>'reset','value'=>'Reset','class'=>'btn btn-primary']); ?> Already Registered...<?php echo anchor('login','Sign In?','class = "link-class"'); ?>
