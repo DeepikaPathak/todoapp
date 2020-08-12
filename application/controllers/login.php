7ad372140e7b364d124f45f25c0cedaf95381738
DeepikaPathak <65222334+DeepikaPathak@users.noreply.github.com>
2020-08-13 02:11:47 +0530

    Update login.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -8,7 +8,6 @@ class Login extends MY_Controller{
     	}
 	}
 
-
 	public function index(){
 		$this->form_validation->set_rules('email','Email','required|valid_email');
 		$this->form_validation->set_rules('password','Pass Word','required|max_length[12]');
@@ -39,20 +38,22 @@ class Login extends MY_Controller{
 	}
 	public function front_validation(){
 		$this->form_validation->set_rules('mobile','Mobile Number','required|numeric');
+		$this->form_validation->set_rules('pincode','Pin Code','required|numeric|exact_length[6]');
+		$this->form_validation->set_rules('name', 'Name','required');
 		$this->form_validation->set_rules('password','Pass Word','required|max_length[12]');
-		$this->form_validation->set_rules('firstname', 'First Name','required|alpha');
-		$this->form_validation->set_rules('lastname', 'Last Name','required|alpha');
 		$this->form_validation->set_rules('email', 'Email','required|valid_email|is_unique[users.email]');
+		$this->form_validation->set_rules('dob', 'Date Of Birth','required');
 		$this->form_validation->set_error_delimiters("<div class='text-danger'>","</div>");
 		if($this->form_validation->run())
 		{
-			$firstname = $this->input->post('firstname');
-			$lastname = $this->input->post('lastname');
+			$name = $this->input->post('name');
+			$pincode = $this->input->post('pincode');
 			$email = $this->input->post('email');
+			$dob = ($this->input->post('dob'));
 			$password = md5($this->input->post('password'));
 			$mobile = $this->input->post('mobile');
 			$this->load->model('loginmodel');
-			if($this->loginmodel->add_user($firstname,$lastname,$email,$password,$mobile)){
+			if($this->loginmodel->add_user($name,$email,$mobile,$dob,$pincode,$password)){
 				$this->session->set_flashdata('user','user inserted successfully');
 				$this->session->set_flashdata('user_class','alert-success');
 			}
